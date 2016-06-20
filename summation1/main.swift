01ae731ad638f86a67a618792564307a38b49d2e
20141105071 <20141105071wyr@mac26.local>
2016-06-20 10:31:48 +0800

    Initial Commit

---
+++ summation1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  summation1
+//
+//  Created by 20141105071 on 16/6/20.
+//  Copyright © 2016年 20141105071. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
